1286d7518a6a74dd8da6ef89418e02d9a849efad
ZeroBall526 <96288248+ZeroBall526@users.noreply.github.com>
2024-07-26 16:23:43 +0900

    feat : bug fix & change balance

--- src/main/kotlin/io/steaming/example/event/Event.kt
+++ src/main/kotlin/io/steaming/example/event/Event.kt
@@ -3,6 +3,7 @@ package io.steaming.example.event
 import net.kyori.adventure.audience.Audience
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.TextColor
+import net.kyori.adventure.text.format.TextDecoration
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
@@ -14,14 +15,14 @@ class Event : Listener {
     fun dmg(event : EntityDamageEvent) {
         if (event.entity is Player){
             val p : Player = event.entity as Player
-            val range = (event.damage.toInt()..20).random().toDouble()
+            val range = (event.damage.toInt()..event.damage.toInt()+20).random().toDouble()
 
             event.damage = range
 
             val aud : Audience = p
-            aud.sendActionBar(Component.text("[받은 데미지 :")
-                .append(Component.text(range, TextColor.color(0xFF0000)))
-                .append(Component.text("]")))
+            aud.sendActionBar(Component.text("[ 받은 데미지 :")
+                .append(Component.text(range, TextColor.color(0xFF0000), TextDecoration.BOLD))
+                .append(Component.text(" ]")))
         }
     }
 }
